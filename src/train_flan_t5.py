db9f6fb1e70f49dfcbbafec58d361661e7332966
lzc-ust <lizhicheng_hkust@163.com>
2025-04-30 17:13:02 +0800

    update

--- src/train_flan_t5.py
+++ src/train_flan_t5.py
@@ -78,7 +78,7 @@ def main():
         logging_dir=logs_dir,
         logging_steps=50,
         predict_with_generate=True,
-        fp16=True,
+        fp16=False,
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
         greater_is_better=False,
